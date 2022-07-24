281f85fad8e98006b130c86ea9320ec18a15d2b1
DavidCWHung <david.cw.hung@gmail.com>
2022-07-23 19:34:23 -0500

    Created Placeholder Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { PlaceholderComponent } from './main/placeholder/placeholder.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    PlaceholderComponent
   ],
   imports: [
     BrowserModule,

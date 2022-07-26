326a7a8995338026ecf0e09516fe451c3bbdf656
DavidCWHung <david.cw.hung@gmail.com>
2022-07-25 19:56:02 -0500

    Added DataStorageService to save and fetch conversation data from Firebase via HTTP request

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Conversation } from "./conversation.model";
+import { ConversationService } from "./conversation.service";
+
+@Injectable({providedIn: 'root'})
+export class DataStorageService {
+    constructor(private http: HttpClient,
+                private conversationService: ConversationService) {}
+
+    storeConversations() {
+        const conversations = this.conversationService.getConversations();
+
+        this.http.put('https://ng-whatsapp-clone-6b6e1-default-rtdb.firebaseio.com/conversations.json', conversations).subscribe(response => {
+            console.log(response);
+        });
+    }
+
+    fetchConversations() {
+        this.http.get<Conversation[]>('https://ng-whatsapp-clone-6b6e1-default-rtdb.firebaseio.com/conversations.json').subscribe(conversations => {
+            this.conversationService.setConversations(conversations);
+        });
+    }
+}
